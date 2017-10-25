fe93c3b74d495d28d970ecdee0d8fdf017e2175b
ryflamm <33077308+ryflamm@users.noreply.github.com>
2017-10-24 20:09:46 -0600

    Update 9-13-2.py

--- 9-13-2.py
+++ 9-13-2.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python
 
 ##################################################################
-#Ryan Flamm
-#9-13
-
-#This code uses Molecular Dynamics to study the melting point of
-#a 3D system.
+# Ryan Flamm
+# This code uses Molecular Dynamics to study the melting point of a 3D system.
 ##################################################################
 
 from mpl_toolkits.mplot3d import Axes3D
@@ -16,18 +13,18 @@ from numpy.linalg import norm
 
 ##################################################################
 
-#calculates net force due to all the particle interactions
+# Calculates net force due to all the particle interactions
 def getForce(positions,particle,sizeX,sizeY,sizeZ):
     fX = 0
     fY = 0
     fZ = 0
-    #find components of r vector
+    # Find components of r vector
     for i in positions:
         xDiff = (i - particle)[0]
         yDiff = (i - particle)[1]
         zDiff = (i - particle)[2]
 
-        #establishes periodic boundaries
+        # Establishes periodic boundaries
         if abs(xDiff) > sizeX/2. and abs(yDiff) > sizeY/2. and abs(zDiff) > sizeZ/2.:
             modifiedPos = array([i[0] - sign(xDiff) * sizeX, i[1] - sign(yDiff) * sizeY, i[2] - sign(zDiff) * sizeZ])
         elif abs(xDiff) > sizeX/2. and abs(yDiff) > sizeY/2.:
@@ -45,14 +42,14 @@ def getForce(positions,particle,sizeX,sizeY,sizeZ):
         else:
             modifiedPos = copy(i)
 
-        #redefines components using the shortest interparticle distance
+        # Redefines components using the shortest interparticle distance
         xDiff = (particle - modifiedPos)[0]
         yDiff = (particle - modifiedPos)[1]
         zDiff = (particle - modifiedPos)[2]
         distance = norm(particle - modifiedPos)
 
-        #establishes the strength of the force using Lennard-Jones potential
-        #these bounds are used to cut unnecessary calculations above r=3
+        # Establishes the strength of the force using Lennard-Jones potential
+        # These bounds are used to cut unnecessary calculations above r=3
         if 0.1 <= distance <= 3.:
             fX += 24. * (3./distance**13 - 1./distance**7) * xDiff/distance
             fY += 24. * (3./distance**13 - 1./distance**7) * yDiff/distance
@@ -60,12 +57,12 @@ def getForce(positions,particle,sizeX,sizeY,sizeZ):
 
     return fX,fY,fZ
 
-#sets up the initial conditions of the particles
+# Sets up the initial conditions of the particles
 def initializeParticles(lVecs,sizeX,sizeY,sizeZ):
     positions = []
     velocities = []
     deltaR = 0.1
-    v0 = 10            #initial velocity
+    v0 = 10            # Initial velocity
 
     for i in range(0,sizeX):
         for j in range(0,sizeY):
@@ -74,10 +71,10 @@ def initializeParticles(lVecs,sizeX,sizeY,sizeZ):
                 velocities.append([2 * (uniform(0,1) - 0.5) * v0, 2 * (uniform(0,1) - 0.5) * v0, 2 * (uniform(0,1) - 0.5) * v0])
     return array(positions),array(velocities)
 
-sizeX = 4                             #size of box in x direction
-sizeY = 4                             #size of box in y direction
-sizeZ = 4                             #size of box in z direction
-nParticles = sizeX * sizeY * sizeZ    #number of particles found in the box
+sizeX = 4                             # Size of box in x direction
+sizeY = 4                             # Size of box in y direction
+sizeZ = 4                             # Size of box in z direction
+nParticles = sizeX * sizeY * sizeZ    # Number of particles found in the box
 time = 0
 tMax = 16
 dt = 0.001
@@ -87,10 +84,10 @@ lVecs = array([[1.,0,0],[0,1.,0],[0,0,1.]])
 
 positions,velocities = initializeParticles(lVecs,sizeX,sizeY,sizeZ)
 
-#sets current conditions to that found in InitialParticles
+# Sets current conditions to that found in InitialParticles
 positionsC = copy(positions)
 
-#previous positions
+# Previous positions
 positionsP = positions - velocities * dt
 
 
@@ -105,8 +102,6 @@ while time < tMax:
         ax.scatter([n[0] for n in positions],[n[1] for n in positions],[n[2] for n in positions])
         plt.draw()
         plt.pause(0.000001)
-#        if time < 13:
-#            plt.clf()
 
     forces = []
 
@@ -114,7 +109,7 @@ while time < tMax:
         fX,fY,fZ = getForce(positions,positions[i],sizeX,sizeY,sizeZ)
         forces.append([fX,fY,fZ])
 
-        #verlet method
+    # Verlet method
     for i in range(len(positions)):
         prevX = positionsP[i][0]
         currentX = positionsC[i][0]
@@ -125,15 +120,18 @@ while time < tMax:
         fX = forces[i][0]
         fY = forces[i][1]
         fZ = forces[i][2]
-        #update positions
+        
+        # Update positions
         positions[i][0] = 2. * currentX - prevX + fX * dt**2
         positions[i][1] = 2. * currentY - prevY + fY * dt**2
         positions[i][2] = 2. * currentZ - prevZ + fZ * dt**2
-        #update velocities
+        
+        # Update velocities
         velocities[i][0] = (positions[i][0] - prevX)/(2. * dt)
         velocities[i][1] = (positions[i][1] - prevY)/(2. * dt)
         velocities[i][2] = (positions[i][2] - prevZ)/(2. * dt)
-        #periodic boundary conditions
+        
+        # Periodic boundary conditions
         if positions[i][0] > sizeX:
             positions[i][0] = positions[i][0] - sizeX
         if positions[i][0] < 0:
@@ -149,8 +147,8 @@ while time < tMax:
 
     time += dt
     index += 1
-    #turn current positions into the new previous positions
+    # Turn current positions into the new previous positions
     positionsP = copy(positionsC)
-    #update current positions
+    # Update current positions
     positionsC = copy(positions)
 plt.show() 
